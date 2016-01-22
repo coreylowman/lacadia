6b93f9d4842ddc304747cab608f0137455f516c1
Corey Lowman <coreylowman@users.noreply.github.com>
2016-01-22 09:21:42 -0500

    Create wall.h

---
+++ src/game/wall.h
@@ -0,0 +1,18 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include "game_object.h"
+#include "collidable_object.h"
+
+typedef struct {
+    GameObject *base;
+    CollidableObject collidable;
+    RenderableObject renderable;
+} Wall;
+
+Wall *wall_new(GameWorld *world);
+void wall_free(World *self);
+
+void wall_on_collide(GameObject *self, GameObject *other);
+
+#endif
