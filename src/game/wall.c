51fc0a83960589a0d064169170ff97b575063365
Corey Lowman <coreylowman@users.noreply.github.com>
2016-01-22 09:25:04 -0500

    Create wall.c

---
+++ src/game/wall.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "wall.h"
+
+Wall *wall_new(GameWorld *world){
+    Wall *self = malloc(sizeof(*self));
+    self->base = game_object_new(world);
+    
+    //TODO set up renderable
+    //TODO set up collidable
+    
+    return self;
+}
+
+void wall_free(World *self){
+    game_object_free(self->base);
+    free(self);
+}
+
+void wall_on_collide(GameObject *self, GameObject *other){
+    //do nothing
+}
