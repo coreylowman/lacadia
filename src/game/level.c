45b1d4a0ff7d98dbbafdc185082589d54f3155b1
Corey Lowman <clowman1993@gmail.com>
2016-02-13 16:48:30 -0500

    Fixing collidable in wall

--- src/game/level.c
+++ src/game/level.c
@@ -12,13 +12,14 @@ Level *level_new(GameWorld *world){
 
 
     self->num_walls = 4;
-    self->walls[0] = wall_new(world, (Vec3){ .data = { dims.x * -5, 0.0, dims.z * 5} }, VEC3_UNIT_X, 10);
-    self->walls[1] = wall_new(world, (Vec3){ .data = { dims.x * -5, 0.0, dims.z * -5} }, VEC3_UNIT_X, 10);
-    self->walls[2] = wall_new(world, (Vec3){ .data = { dims.x * -5, 0.0, dims.z * -4} }, VEC3_UNIT_Z, 9);
-    self->walls[3] = wall_new(world, (Vec3){ .data = { dims.x * 5, 0.0, dims.z * -4} }, VEC3_UNIT_Z, 9);
+    self->walls[0] = wall_new(world, (Vec3){ .data = { dims.x * -5, 0.0, dims.z * -5} }, VEC3_UNIT_X, 10);
+	self->walls[1] = wall_new(world, (Vec3){ .data = { dims.x * 5, 0.0, dims.z * -5} }, VEC3_UNIT_Z, 10);
+	self->walls[2] = wall_new(world, (Vec3){ .data = { dims.x * -4, 0.0, dims.z * 5} }, VEC3_UNIT_X, 10);
+    self->walls[3] = wall_new(world, (Vec3){ .data = { dims.x * -5, 0.0, dims.z * -4} }, VEC3_UNIT_Z, 10);
+    
     int i;
     for(i = 0;i < self->num_walls;i++){
-        game_world_add_wall(world, self->walls[0], i);
+        game_world_add_wall(world, self->walls[i], i);
     }
     return self;
 }
--- src/game/wall.c
+++ src/game/wall.c
@@ -1,39 +1,45 @@
 #include <stdlib.h>
 #include "wall.h"
 #include "util/mat4.h"
+#include "util/random.h"
 
 Wall *wall_new(GameWorld *world, Vec3 position, Vec3 grow_direction, int length){
     Wall *self = malloc(sizeof(*self));
     self->base_object = game_object_new(world, GAME_OBJECT_TYPE_WALL);
     self->base_object->container = self;
 
-	int asset_id = game_world_get_asset_id(world, "assets/wall");
+	Vec3 dims = wall_dimensions(world);
+	int which = grow_direction.x ? 0 : (grow_direction.y ? 1 : 2);
+	float width = grow_direction.x ? dims.x : (grow_direction.y ? dims.y : dims.z);
 
-    float width;
-    self->collidable.on_collide = wall_on_collide;
-    self->collidable.is_colliding = collidable_object_is_colliding;
-    self->collidable.container = self->base_object;
-    self->collidable.bounding_box = game_world_get_asset_obb(world, asset_id);
-    self->collidable.bounding_box.center = position;
-    self->collidable.bounding_box.center.y += self->collidable.bounding_box.radius.y;
-    width = 2 * self->collidable.bounding_box.radius.x;
-
-    self->num_renderables = length;
+    int asset_id = game_world_get_asset_id(world, "assets/wall");
     int i;
-    Mat4 model_matrix;
     Vec3 pos;
+    Mat4 model_matrix;
+    self->num_renderables = length;
     for(i = 0;i < length;i++){
         pos = vec3_add(position, vec3_scale(grow_direction, width * (float)i));
         mat4_ident(&model_matrix);
-        mat4_translate(&model_matrix, pos);
+		mat4_rotate_y(&model_matrix, 3.14159265358979323846 * 0.5 * (float)random_in_rangei(0, 4));
+        mat4_translate(&model_matrix, pos);		
         mat4_transpose(&model_matrix);
 
         self->renderables[i].asset_id = asset_id;
         self->renderables[i].model_matrix = model_matrix;
     }
 
-    //TODO this only sets up a collidable that contains 1 wall segement...
-    //want it to contain length segments
+    
+    
+    self->collidable.bounding_box = game_world_get_asset_obb(world, asset_id);
+    self->collidable.bounding_box.center = position;
+    self->collidable.bounding_box.center.y += dims.y * 0.5;
+	self->collidable.bounding_box.radius.data[which] *= length;
+	self->collidable.bounding_box.center.data[which] += width * (length - 1)* 0.5;
+    
+    self->collidable.container = self->base_object;
+
+    self->collidable.on_collide = wall_on_collide;
+    self->collidable.is_colliding = collidable_object_is_colliding;
     return self;
 }
 
