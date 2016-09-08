a744658d5d721edf5a7a6a0b41c60cc07683f6ac
Corey Lowman <clowman1993@gmail.com>
2016-09-08 11:58:52 -0400

    Updating text shaders to fit in better

--- shaders/text_frag.glsl
+++ shaders/text_frag.glsl
@@ -1,12 +1,13 @@
 #version 400
 
-in vec2 TexCoords;
+uniform sampler2D text;
+
+in vec2 texture_coords;
+in vec3 fragment_color;
+
 out vec4 color;
 
-uniform sampler2D text;
-uniform vec3 textColor;
-
 void main() {
-    vec4 sampled = vec4(1.0, 1.0, 1.0, texture2D(text, TexCoords).r);
-    color = vec4(textColor, 1.0) * sampled;
+    vec4 sampled = vec4(1.0, 1.0, 1.0, texture2D(text, texture_coords).r);
+    color = vec4(fragment_color, 1.0) * sampled;
 }
--- shaders/text_vert.glsl
+++ shaders/text_vert.glsl
@@ -1,9 +1,14 @@
 #version 400
 
 layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
-out vec2 TexCoords;
+layout (location = 1) in vec3 text_color;
+
+out vec2 texture_coords;
+out vec3 fragment_color;
 
 void main() {
     gl_Position = vec4(vertex.xy, 0.0, 1.0);
-    TexCoords = vertex.zw;
+
+    texture_coords = vertex.zw;
+    fragment_color = text_color;
 }
